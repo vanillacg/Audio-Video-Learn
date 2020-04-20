206aacf586cff540c8c23a6fa7afc998b0ced6ab
chenguang <guang_chen@Ctrip.com>
2020-04-20 15:39:45 +0800

    Initial Commit

---
+++ LearnAVone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnAVone
+//
+//  Created by chenguang on 2020/4/20.
+//  Copyright © 2020 chenguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LearnAVone/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LearnAVone
+//
+//  Created by chenguang on 2020/4/20.
+//  Copyright © 2020 chenguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
